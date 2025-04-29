33d521712e9e4cfef6771c6d21643a2402ceeffd
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-29 10:04:20 +0500

    refactor: Add Block constructor and proof of work implementation

--- src/block.rs
+++ src/block.rs
@@ -10,3 +10,21 @@ pub struct Block {
 pub struct Blockchain {
     blocks: Vec<Block>,
 }
+
+impl Block {
+    pub fn new(data: String, prev_block_hash: String, height: usize) -> Result<Self> {
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis();
+        let mut block = Block {
+            timestamp,
+            transactions: data,
+            prev_block_hash,
+            hash: String::new(),
+            height,
+            nonce: 0,
+        };
+        block.run_proof_of_work()?;
+        Ok(block)
+    }
+}
