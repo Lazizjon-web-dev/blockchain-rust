6361be080d8671aff8046ecda97081fef0e0fd39
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-29 10:15:36 +0500

    refactor: Improve proof of work implementation in Block
    This commit refactors the `Block` struct to improve the implementation of the proof of work algorithm. The `run_proof_of_work` method has been modified to increment the `nonce` value until a valid hash is found. The `prepare_hash_data` method has been added to serialize the block's content for hashing. The `validate` method has been updated to compare the first `TARGET_LEN` characters of the hash with a target string. These changes enhance the mining process and ensure the integrity of the blockchain.

--- src/block.rs
+++ src/block.rs
@@ -27,4 +27,38 @@ impl Block {
         block.run_proof_of_work()?;
         Ok(block)
     }
+
+    fn run_proof_of_work(&mut self) -> Result<()> {
+        info!("Mining the block");
+        while !self.validate()? {
+            self.nonce += 1;
+        }
+        let data = self.prepare_hash_data();
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        self.hash = hasher.result_str();
+        Ok(())
+    }
+
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content = (
+            self.prev_block_hash.clone(),
+            self.transactions.clone(),
+            self.timestamp,
+            TARGET_LEN,
+            self.nonce,
+        );
+        let bytes = bincode::serialize(&content)?;
+        Ok(bytes)
+    }
+
+    fn validate(&self) -> Result<bool> {
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        let mut vec1: Vec<u8> = vec![];
+        vec1.resize(TARGET_LEN, 0 as u8);
+        println!("vec1: {:?}", vec1);
+        Ok(&hasher.result_str()[0..TARGET_LEN] == String::from_utf8(&vec1[..])?)
+    }
 }
