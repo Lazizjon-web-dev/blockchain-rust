352d367e6099e66466a8d50b46436c21b76713f6
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-01 05:14:08 +0500

    refactor: Implement command loop in Cli struct for interactive CLI functionality

--- src/cli.rs
+++ src/cli.rs
@@ -12,6 +12,33 @@ impl Cli {
         Ok(Cli { bc: Blockchain::new()?, rl: DefaultEditor::new()? })
     }
     pub fn run(&mut self) -> Result<()> {
-        Ok(())
+        loop {
+            let readline = self.rl.readline(">> ");
+            match readline {
+                Ok(line) if line.trim().is_empty() => {
+                    println!("Empty command, please try again.");
+                    continue;
+                }
+                Ok(line) => {
+                    self.rl.add_history_entry(line.as_str())?;
+                    let line = line.trim();
+                    match line.split_whitespace().nth(0).unwrap_or("") {
+                        "\\q" => {
+                            println!("Exiting CLI...");
+                            break Ok(());
+                        }
+                        // TODO: Add more commands
+                        _ => {
+                            println!("Unknown command: {}", line);
+                            continue;
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error reading line, please try again.");
+                    break Err(e.into());
+                }
+            }
+        }
     }
 }
