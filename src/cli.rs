7e2cbd21192f870f3f261398b17cdac0cfbf07c5
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-01 05:10:01 +0500

    refactor: Initialize DefaultEditor in Cli struct

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,17 @@
 use crate::blockchain::Blockchain;
 use crate::error::Result;
+use rustyline::DefaultEditor;
 
 pub struct Cli {
     bc: Blockchain,
+    rl: DefaultEditor,
 }
 
 impl Cli {
     pub fn new() -> Result<Self> {
-        Ok(Cli { bc: Blockchain::new()? })
+        Ok(Cli { bc: Blockchain::new()?, rl: DefaultEditor::new()? })
+    }
+    pub fn run(&mut self) -> Result<()> {
+        Ok(())
     }
 }
