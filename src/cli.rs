cdb7680734636c65bbe301bb8ff73c0e7a4adaee
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-03 09:23:24 +0500

    refactor: Simplify CLI command handling by removing interactive loop and integrating clap for command parsing

--- src/cli.rs
+++ src/cli.rs
@@ -1,57 +1,53 @@
 use crate::blockchain::Blockchain;
 use crate::error::Result;
-use clap::arg;
-use rustyline::DefaultEditor;
+use clap::{Command, arg};
 
 pub struct Cli {
     bc: Blockchain,
-    rl: DefaultEditor,
 }
 
 impl Cli {
     pub fn new() -> Result<Self> {
-        Ok(Cli { bc: Blockchain::new()?, rl: DefaultEditor::new()? })
+        Ok(Cli {
+            bc: Blockchain::new()?,
+        })
     }
     pub fn run(&mut self) -> Result<()> {
-        loop {
-            let readline = self.rl.readline(">> ");
-            match readline {
-                Ok(line) if line.trim().is_empty() => {
-                    println!("Empty command, please try again.");
-                    continue;
-                }
-                Ok(line) => {
-                    self.rl.add_history_entry(line.as_str())?;
-                    let line = line.trim();
-                    match line.split_whitespace().nth(0).unwrap_or("") {
-                        "\\q" => {
-                            println!("Exiting CLI...");
-                            break Ok(());
-                        }
-                        "\\print" | "\\p" => {
-                            for block in self.bc.iter() {
-                                println!("Block: {:?}", block);
-                                // TODO: Add more block details
-                            }
-                        }
-                        "\\add" | "\\a" => {
-                            let args = arg!("<data>...").get_matches_from(line.split_whitespace());
-                            let data = args.get_one::<String>("data").unwrap();
-                            self.bc.add_block(data.clone())?;
-                            println!("Added block with data: {}", data);
-                        }
-                        // TODO: Add more commands
-                        _ => {
-                            println!("Unknown command: {}", line);
-                            continue;
-                        }
-                    }
-                }
-                Err(e) => {
-                    println!("Error reading line, please try again.");
-                    break Err(e.into());
-                }
+        let matches = Command::new("Blockchain CLI")
+            .version("1.0")
+            .author("Lazizjon-web-dev")
+            .about("A simple CLI for interacting with a blockchain")
+            .subcommand(Command::new("print").about("Print the blockchain"))
+            .subcommand(
+                Command::new("add")
+                    .about("Add a new block")
+                    .arg(arg!(<DATA>" 'the blockchain data'")),
+            )
+            .get_matches();
+        if let Some(ref matches) = matches.subcommand_matches("add") {
+            if let Some(c) = matches.get_one::<String>("DATA") {
+                self.add_block(c.clone())?;
+                println!("Added block with data: {}", c);
+            } else {
+                println!("No data provided for the block.");
             }
         }
+
+        if let Some(_) = matches.subcommand_matches("print") {
+            self.print_chain()?;
+        }
+
+        Ok(())
+    }
+
+    fn add_block(&mut self, data: String) -> Result<()> {
+        self.bc.add_block(data)?;
+        Ok(())
+    }
+    fn print_chain(&self) -> Result<()> {
+        for block in self.bc.iter() {
+            println!("Block: {:?}", block);
+        }
+        Ok(())
     }
 }
