aeeade72d375fb7f24088584ad5ac34ac6b7d920
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-04 12:34:48 +0500

    refactor: Move hash_pub_key function outside of Wallet implementation for better modularity

--- src/wallet.rs
+++ src/wallet.rs
@@ -35,3 +35,13 @@ impl Wallet {
     }
 
 }
+
+pub fn hash_pub_key(pub_key: &mut Vec<u8>) {
+    let mut hasher1 = Sha256::new();
+    hasher1.input(pub_key);
+    hasher1.result(pub_key);
+    let mut hasher2 = Ripemd160::new();
+    hasher2.input(pub_key);
+    pub_key.resize(20, 0);
+    hasher2.result(pub_key);
+}
