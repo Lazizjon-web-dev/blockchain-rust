6921b6a2671889a1d770a93490776d2c66f42f09
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-04 11:30:48 +0500

    refactor: Enhance new_UTXO method for improved transaction creation and error handling

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,6 +1,8 @@
-use serde::{Serialize, Deserialize};
-use crypto::{sha2::Sha256, digest::Digest};
-use crate::error::Result;
+use crate::{blockchain::Blockchain, error::Result};
+use crypto::{digest::Digest, sha2::Sha256};
+use failure::format_err;
+use log::error;
+use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Transaction {
     pub id: String,
@@ -22,6 +24,50 @@ pub struct TXOutput {
 }
 
 impl Transaction {
+    pub fn new_UTXO(from: &str, to: &str, amount: i32, bc: &Blockchain) -> Result<Self> {
+        let mut vin = Vec::new();
+        let acc_v = bc.find_spendable_outputs(from, amount);
+
+        if acc_v.0 < amount {
+            error!("Not enough funds");
+            return Err(format_err!(
+                "Not enough funds: current balance is {}",
+                acc_v.0
+            ));
+        }
+
+        for tx in acc_v.1 {
+            for out in tx.1 {
+                let input = TXInput {
+                    txid: tx.0.clone(),
+                    vout: out,
+                    script_sig: String::from(from),
+                };
+                vin.push(input);
+            }
+        }
+
+        let mut vout = vec![TXOutput {
+            value: amount,
+            script_pub_key: String::from(to),
+        }];
+
+        if acc_v.0 > amount {
+            vout.push(TXOutput {
+                value: acc_v.0 - amount,
+                script_pub_key: String::from(from),
+            });
+        }
+
+        let mut tx = Transaction {
+            id: String::new(),
+            vin,
+            vout,
+        };
+        tx.set_id()?;
+        Ok(tx)
+    }
+
     pub fn new_coinbase(to: String, mut data: String) -> Result<Self> {
         if data == String::from("") {
             data += &format!("Reward to {}", to);
@@ -29,12 +75,12 @@ impl Transaction {
 
         let mut tx = Transaction {
             id: String::new(),
-            vin: vec![ TXInput {
+            vin: vec![TXInput {
                 txid: String::new(),
                 vout: -1,
                 script_sig: data,
             }],
-            vout: vec![ TXOutput {
+            vout: vec![TXOutput {
                 value: 100,
                 script_pub_key: to,
             }],
@@ -49,7 +95,7 @@ impl Transaction {
     }
 
     fn set_id(&mut self) -> Result<()> {
-        let mut hasher= Sha256::new();
+        let mut hasher = Sha256::new();
         let data = bincode::serialize(self)?;
         hasher.input(&data[..]);
         self.id = hasher.result_str();
