5f166d1c3faa1a0ca8f22c60e950954475017d5a
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-04 11:20:14 +0500

    refactor: Update add_block method to accept a vector of transactions for improved block creation

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -50,10 +50,10 @@ impl Blockchain {
         Ok(bc)
     }
 
-    pub fn add_block(&mut self, data: String) -> Result<()> {
+    pub fn add_block(&mut self, transactions: Vec<Transaction>) -> Result<()> {
         let last_hash = self.db.get("LAST")?.unwrap();
         let last_hash = String::from_utf8(last_hash.to_vec())?;
-        let new_block = Block::new(data, last_hash, TARGET_LEN)?;
+        let new_block = Block::new(transactions, last_hash, TARGET_LEN)?;
         self.db.insert(new_block.get_hash(), bincode::serialize(&new_block)?)?;
         self.db.insert("LAST", new_block.get_hash().as_bytes())?;
         self.current_hash = new_block.get_hash();
