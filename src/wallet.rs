7fcde5963d50499da613ec3180d51ed8dc8e151f
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-04 12:17:20 +0500

    feat: Create Wallet struct for managing secret and public keys

---
+++ src/wallet.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Wallet {
+    pub secret_key: Vec<u8>,
+    pub public_key: Vec<u8>,
+}
