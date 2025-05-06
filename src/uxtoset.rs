ddc5a91a801e4c20525e025f8f6fed96164d2704
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-06 11:11:06 +0500

    feat: Implement reindex method in UTXOSet for UTXO database management

--- src/uxtoset.rs
+++ src/uxtoset.rs
@@ -1,5 +1,23 @@
-use crate::blockchain::Blockchain;
+use crate::{blockchain::Blockchain, error::Result};
+use std::fs::remove_dir_all;
+use sled;
 
 pub struct UTXOSet {
     pub blockchain: Blockchain,
+}
+
+impl UTXOSet {
+    pub fn reindex(&self) -> Result<()> {
+        remove_dir_all("data/utxos")?;
+        let db = sled::open("data/utxos")?;
+
+        let utxos = self.blockchain.find_UTXO()?;
+
+        for (txid, outs) in utxos {
+            db.insert(txid.as_bytes(), bincode::serialize(&outs)?)?;
+        }
+
+        db.flush()?;
+        Ok(())
+    }
 }
