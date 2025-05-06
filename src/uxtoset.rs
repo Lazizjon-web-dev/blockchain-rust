ed05a2b357d5a955f6626f32d5e7f68f0dbfa109
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-06 11:20:43 +0500

    feat: Add update method to UTXOSet for processing block transactions

--- src/uxtoset.rs
+++ src/uxtoset.rs
@@ -1,6 +1,6 @@
-use crate::{blockchain::Blockchain, error::Result};
-use std::fs::remove_dir_all;
+use crate::{blockchain::Blockchain, error::Result, tx::TXOutput};
 use sled;
+use std::fs::remove_dir_all;
 
 pub struct UTXOSet {
     pub blockchain: Blockchain,
@@ -15,6 +15,44 @@ impl UTXOSet {
 
         for (txid, outs) in utxos {
             db.insert(txid.as_bytes(), bincode::serialize(&outs)?)?;
+        }
+
+        db.flush()?;
+        Ok(())
+    }
+
+    pub fn update(&self, block: &Block) -> Result<()> {
+        let db = sled::open("data/utxos")?;
+
+        for tx in block.get_transactions() {
+            if !tx.is_coinbase() {
+                for vin in &tx.vin {
+                    let mut update_outputs = TXOutputs {
+                        outputs: Vec::new(),
+                    };
+                    let outs: TXOutputs = bincode::deserialize(&db.get(&vin.txid)?.unwrap())?;
+                    for out_idx in 0..outs.outputs.len() {
+                        if out_idx != vin.vout as usize {
+                            update_outputs.outputs.push(outs.outputs[out_idx].clone());
+                        }
+                    }
+
+                    if update_outputs.outputs.is_empty() {
+                        db.remove(&vin.txid)?;
+                    } else {
+                        db.insert(vin.txid.as_bytes(), bincode::serialize(&update_outputs)?)?;
+                    }
+                }
+            }
+            let mut new_outputs = TXOutputs {
+                outputs: Vec::new(),
+            };
+
+            for out in &tx.vout {
+                new_outputs.outputs.push(out.clone());
+            }
+
+            db.insert(tx.id.as_bytes(), bincode::serialize(&new_outputs)?)?;
         }
 
         db.flush()?;
