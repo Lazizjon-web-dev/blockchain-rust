247e544eaad7889482098ce5d111d52b66855a42
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-07 05:29:11 +0500

    feat: Define Server and ServerInner structs for managing node and transaction data

--- src/server.rs
+++ src/server.rs
@@ -2,3 +2,15 @@ const KNOWN_NODE1: &str = "localhost: 3000";
 const CMD_LEN: usize = 12;
 const VERSION: i32 = 1;
 
+pub struct Server {
+    node_address: String,
+    mining_address: String,
+    inner: Arch<Mutex<ServerInner>>,
+}
+
+struct ServerInner {
+    known_nodes: HashSet<String>,
+    utxo: UTXOSet,
+    blocks_in_transit: Vec<String>,
+    mempool: HashMap<String, Transaction>,
+}
