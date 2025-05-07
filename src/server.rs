f1fdca16c49773429693eec997299855e0e80ce0
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-05-07 05:35:56 +0500

    feat: Add message structs for block communication and data retrieval

--- src/server.rs
+++ src/server.rs
@@ -14,3 +14,40 @@ struct ServerInner {
     blocks_in_transit: Vec<String>,
     mempool: HashMap<String, Transaction>,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct BlockMsg {
+    address_from: String,
+    block: Block,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct GetBlocksMsg {
+    address_from: String,
+}
+
+struct GetDataMsg {
+    address_from: String,
+    kind: String,
+    id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct InviteMsg {
+    address_from: String,
+    kind: String,
+    items: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct TransactionMsg {
+    address_from: String,
+    transaction: Transaction,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct VersionMsg {
+    address_from: String,
+    version: i32,
+    best_height: i32,
+}
