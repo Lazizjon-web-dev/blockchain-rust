af66664d407ab75e4908d7d0741f8a3f18437a51
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-05-31 11:27:34 +0500

    chore: Final touches to the block.rs file including fixing errors, formatting the file etc.

--- src/block.rs
+++ src/block.rs
@@ -14,34 +14,33 @@ pub struct Block {
     transactions: Vec<Transaction>,
     prev_block_hash: String,
     hash: String,
-    height: i32,
     nonce: i32,
+    height: i32,
 }
 
 impl Block {
-    pub fn new(data: Vec<Transaction>, prev_block_hash: String, height: i32) -> Result<Self> {
+    pub fn new(
+        transactions: Vec<Transaction>,
+        prev_block_hash: String,
+        height: i32,
+    ) -> Result<Self> {
         let timestamp = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)?
             .as_millis();
         let mut block = Block {
             timestamp,
-            transactions: data,
+            transactions,
             prev_block_hash,
             hash: String::new(),
-            height,
             nonce: 0,
+            height,
         };
         block.run_proof_of_work()?;
         Ok(block)
     }
 
     pub fn new_genesis_block(coinbase: Transaction) -> Self {
-        Block::new(vec![coinbase], String::new(), 0)
-            .unwrap_or_else(|_| panic!("Failed to create genesis block"))
-    }
-
-    pub fn get_height(&self) -> i32 {
-        self.height
+        Block::new(vec![coinbase], String::new(), 0).expect("Failed to create genesis block")
     }
 
     pub fn get_hash(&self) -> String {
@@ -56,22 +55,26 @@ impl Block {
         &self.transactions
     }
 
+    pub fn get_height(&self) -> i32 {
+        self.height
+    }
+
     fn run_proof_of_work(&mut self) -> Result<()> {
         info!("Mining the block");
         while !self.validate()? {
             self.nonce += 1;
         }
-        let data = self.prepare_hash_data().unwrap();
+        let data = self.prepare_hash_data()?;
         let mut hasher = Sha256::new();
         hasher.input(&data[..]);
         self.hash = hasher.result_str();
         Ok(())
     }
 
-    fn hash_transactions(&self) -> Result<Vec<u8>> {
-        let mut transactions: Vec<Vec<u8>> = Vec::new();
+    fn hash_transactions(&mut self) -> Result<Vec<u8>> {
+        let mut transactions = Vec::new();
 
-        for tx in &self.transactions {
+        for tx in &mut self.transactions {
             transactions.push(tx.hash()?.as_bytes().to_owned());
         }
 
@@ -80,7 +83,7 @@ impl Block {
         Ok(tree.root())
     }
 
-    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+    fn prepare_hash_data(&mut self) -> Result<Vec<u8>> {
         let content = (
             self.prev_block_hash.clone(),
             self.hash_transactions()?,
@@ -88,15 +91,15 @@ impl Block {
             TARGET_HEXS,
             self.nonce,
         );
-        let mut bytes: Vec<u8> = bincode::serialize(&content)?;
+        let bytes: Vec<u8> = bincode::serialize(&content)?;
         Ok(bytes)
     }
 
-    fn validate(&self) -> Result<bool> {
+    fn validate(&mut self) -> Result<bool> {
         let data = self.prepare_hash_data()?;
         let mut hasher = Sha256::new();
         hasher.input(&data[..]);
-        let mut vec1: Vec<u8> = vec![];
+        let mut vec1: Vec<u8> = Vec::new();
         vec1.resize(TARGET_HEXS, 0 as u8);
         println!("vec1: {:?}", vec1);
         Ok(&hasher.result_str()[0..TARGET_HEXS] == String::from_utf8(vec1)?)
